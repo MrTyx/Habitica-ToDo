8f24224e27a235d786a8cff318986add13d3a5e4
MrTyx <camerontyrel@gmail.com>
2016-06-18 22:49:02 +1000

    Changed the handling of date
    Changed date from local time to UTC time

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,13 @@
 $(function() {
   chrome.storage.sync.get([
-    "habitica_todo_user_id",
-    "habitica_todo_api_token",
-    "habitica_todo_difficulty",
-    "habitica_todo_show_options",
-    "habitica_todo_autoclose_tab"
+    "habitica_todo_user_id",            // User ID for authentication
+    "habitica_todo_api_token",          // API token as password for User ID
+    "habitica_todo_difficulty",         // String of difficulty, eg 'medium'
+    "habitica_todo_show_options",       // Show options on icon click? eg 'yes'
+    "habitica_todo_autoclose_tab"       // Close tab after success? eg 'no'
   ], function(items) {
-
     if (!chrome.runtime.error) {
+      // If you are missing a setting, open newtab to options.
       if (
         !items.habitica_todo_user_id      ||
         !items.habitica_todo_api_token    ||
@@ -16,14 +16,17 @@ $(function() {
         !items.habitica_todo_autoclose_tab) {
         chrome.tabs.create({'url': '/options_page.html'});
       } else {
+        // Get the current window behind the popup
+        // We need this for the url, the title, and the id (to autoclose it)
         chrome.tabs.query({
           active: true,
           currentWindow: true
         }, function(tab) {
-          items.tab_title = tab[0].title;
-          items.tab_url   = tab[0].url;
-          items.tab_id    = tab[0].id;
+          items.tab_title = tab[0].title;       // eg 'Facebook'
+          items.tab_url   = tab[0].url;         // eg 'http://www.facebook.com'
+          items.tab_id    = tab[0].id;          // eg '123'
           items.due_date  = '';
+          // If 'Don't show' setting, then just show the loader and post
           if (items.habitica_todo_show_options == 'no') {
             $("body").load("loader.html", function() {
               post_data(items);
@@ -39,21 +42,27 @@ $(function() {
 
 function prepare_form(items) {
   $("body").load("popup_form.html", function() {
+    // Populate the form with some defaults.
+    // Jquery lets us trigger 'clicks' on radios so the right default is selected
     $("#title").val(items.tab_title);
     $("#url").val(items.tab_url);
     $("#difficulty_radios").buttonset();
     $("input:radio[name=difficulty]").filter("[value="+items.habitica_todo_difficulty+"]")
                                      .trigger("click");
     $("#date").datepicker({
-      dateFormat: "yy/mm/dd"
+      dateFormat: "yy-mm-dd"
     });
 
     $("#send_button").on("click", function() {
+      // Wrapper covers the entire area, inner only covers a strip.
+      // This is so we can have only 1 loader, but different heights
       $('#loading_wrapper').show();
       $('#loading_inner').load('loader.html');
+
+      // Update the array values to match the values in the input fields
       items.tab_title = $('#title').val();
       items.tab_url   = $('#url').val();
-      items.due_date  = $('#date').val();
+      items.due_date  = $('#date').val(); // + 'T14:00:00.000Z'
       items.habitica_todo_difficulty = $('input:radio[name=difficulty]:checked').val();
       post_data(items);
     })
@@ -62,14 +71,14 @@ function prepare_form(items) {
 
 function post_data(items){
 
-
   // Remove ] and ) where it would break Habitica markdown
   var url   = items.tab_url.split(')').join('%29');
   var title = items.tab_title.split(']').join('\]')
                              .split('[').join('\[');
 
   // Change relative URLs to absolute.
-  if (/^[a-zA-Z0-9_\-]+:\/\//.test(url) == false) {
+  // If url doesn't begin with 'WORD://WHATEVER'
+  if (!/^[a-zA-Z0-9_\-]+:\/\//.test(url)) {
     url = 'http://' + url;
   }
 
@@ -80,6 +89,8 @@ function post_data(items){
   xhr.setRequestHeader("x-api-key", items.habitica_todo_api_token);
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
+      // Habitica uses 201, but v2 used 200, so this is mostly just incase
+      // they change something in the future
       if (xhr.status == 200 || xhr.status == 201) {
         new Audio('sounds/success_1.mp3').play();
         setTimeout(function(){
@@ -95,6 +106,8 @@ function post_data(items){
       }
     }
   }
+
+  // Difficulty is stored in Chrome Sync as string, so we adjust to number here
   var difficulty = 1;
   switch(items.habitica_todo_difficulty) {
     case 'trivial': difficulty = 0.1; break;
@@ -103,11 +116,27 @@ function post_data(items){
     case 'hard':    difficulty = 2;   break;
     default:        difficulty = 1;   break;
   }
+
+  // Adjust date from local timezone to UTC, then to an ISOString when posted
+  var due_date = new Date(items.due_date);
+  var adjusted_date = new Date(
+    Date.UTC(
+      due_date.getFullYear(),
+      due_date.getMonth(),
+      due_date.getDate(),
+      due_date.getHours(),
+      due_date.getMinutes(),
+      due_date.getSeconds(),
+      due_date.getMilliseconds()
+    )
+  );
+
+  // Finally post the formatted data.
   xhr.send(JSON.stringify({
     "text": "["+title+"]("+url+" )",
     "type":"todo",
     "value":"0",
     "priority": difficulty,
-    "date": items.due_date
+    "date": adjusted_date.toISOString()
   }));
 }
